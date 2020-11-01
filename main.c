7046652db10706ac4b85c58dacfcc7c98b8267c4
bastien707 <bastien.facqueur@efrei.net>
2020-11-01 11:08:38 +0100

    include added

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+#include "UseHugeNumber/Operator/HugeFloatOperator.h"
+#include "UseHugeNumber/Operator/HugeIntOperator.h"
+#include "UseHugeNumber/Operator/HugeUnsignedIntOperator.h"
+
 int main (void) {
 	printf("HUGE NUMBER CALCULATOR\n");
     return EXIT_SUCCESS;
